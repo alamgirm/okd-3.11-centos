58716ae41ff9c16d2a3f0a4042bd3f9b56f5f26c
alamgirm <alamgirm@users.noreply.github.com>
2020-08-15 19:00:56 -0400

    Create basic-prep.sh

---
+++ scripts/basic-prep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Basic preparation, run on each of the nodes both master and worker
+
+source settings.sh
+
+cat >>/etc/hosts<<EOF
+${OKD_MASTER_IP} ${OKD_MASTER_HOSTNAME} console console.${DOMAIN}
+${OKD_WORKER_NODE_1_IP} ${OKD_WORKER_NODE_1_HOSTNAME}
+${OKD_WORKER_NODE_2_IP} ${OKD_WORKER_NODE_2_HOSTNAME}
+EOF
+
+# install updates
+yum update -y
+
+# install the following base packages
+yum install -y wget figlet zile nano net-tools docker-1.13.1\
+ bind-utils iptables-services bridge-utils bash-completion kexec-tools sos\
+ psacct openssl-devel httpd-tools NetworkManager python-cryptography\
+ python2-pip python-devel python-passlib\
+ java-1.8.0-openjdk-headless "@Development Tools" epel-release
+
+# Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
+sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+
+systemctl | grep "NetworkManager.*running"
+if [ $? -eq 1 ]; then
+        systemctl start NetworkManager
+        systemctl enable NetworkManager
+fi
+
+systemctl restart docker
+systemctl enable docker
